080772020d798c0fa6270aadac9558686a3e174b
Gideon Chrapko <gideonchrapko@Gideons-MBP.hitronhub.home>
2021-12-01 15:08:17 -0700

    dunno whats going on

--- src/Components/singlePost.js
+++ src/Components/singlePost.js
@@ -8,7 +8,7 @@ import {motion} from 'framer-motion';
 const SinglePost = () => {
   const [singlePost, setSinglePost] = useState(null);
   const { slugRoute } = useParams()
-	const [rotate, setRotate] = useState()
+	// const [rotate, setRotate] = useState()
 
   useEffect(() => {
     sanityClient.fetch(`*[slugRoute.current == "${slugRoute}"]{
